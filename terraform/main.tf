8b4a461d7c89f9724ecdd89451b2dfe9ee6ee479
Dmytro Mulenko <mula.d.m@gmail.com>
2022-01-04 19:25:21 +0200

    create vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,21 @@
-#test
+# VPC
+# https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.11.0"
+  name = var.name
+  cidr = var.vpc_cidr
+
+  # The number of public subnet CIDR blocks specified in public_subnets must be greater
+  # than or equal to the number of availability zones specified in var.azs.
+  # This is to ensure that each NAT Gateway has a dedicated public subnet to deploy to.
+  azs             = var.availability_zones
+
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  # enable_nat_gateway = true
+  # enable_vpn_gateway = true
+
+  tags = local.tags
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,47 @@
 variable "name" {
   type    = string
-  default = ""
+  default = "demo"
 }
 
-variable "location" {
+variable "region" {
   type    = string
-  default = ""
+  default = "us-east-1"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  default = [
+    "us-east-1a",
+    "us-east-1b",
+    "us-east-1c",
+  ]
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "vpc_public_subnets" {
+  type        = list(string)
+  default = [
+    "10.0.101.0/24",
+    "10.0.102.0/24",
+  ]
+}
+
+variable "vpc_private_subnets" {
+  type        = list(string)
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+  ]
+}
+
+locals {
+  tags = {
+    "Project"    = var.name
+    "Created_By" = "Terraform"
+    "Notes"      = "Do not edit it manually!"
+  }
 }
