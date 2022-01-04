3798bdda05b1ffa83a197e91f2a9f633740dd3cc
Dmytro Mulenko <mula.d.m@gmail.com>
2022-01-04 19:45:49 +0200

    add sg

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,3 +19,34 @@ module "vpc" {
 
   tags = local.tags
 }
+
+#Confogure security group
+resource "aws_security_group" "allow_web" {
+  name        = join("_", [var.name, "allow_web"])
+  description = "Allow http and https inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description      = "HTTPS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = local.tags
+}
