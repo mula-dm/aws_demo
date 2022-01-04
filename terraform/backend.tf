e11cb266051ed6b436e86ea272fd43eb0dcce813
Dmytro Mulenko <mula.d.m@gmail.com>
2022-01-04 18:49:19 +0200

    initial commit

---
+++ terraform/backend.tf
@@ -0,0 +1,13 @@
+
+# Backend configuration
+# https://www.terraform.io/language/settings/backends/s3
+
+# Now it disabled because it's just POC!
+
+# terraform {
+#   backend "s3" {
+#     bucket = "aws_demo"
+#     key    = "terraform"
+#     region = "us-east-1"
+#   }
+# }
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  type    = string
+  default = ""
+}
+
+variable "location" {
+  type    = string
+  default = ""
+}
